94aba5720fc1fc04923186c5f867f23a95ae3941
preem <s6306021632032@email.kmutnb.ac.th>
2026-01-12 16:21:39 +0700

    Update UI MungMink Shop and Fix Dashboard Error

--- Mungminkshop/settings.py
+++ Mungminkshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nsy0hwa!e&bsej6s#r42d)k45pw!@ejj@e^)3g#qtadepg%*b0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yourusername.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
